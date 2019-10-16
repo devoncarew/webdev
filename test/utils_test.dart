509a3d7b8a93d8e08666b07186af99ae2166ccb8
Devon Carew <devoncarew@google.com>
2019-10-16 15:32:01 -0700

    updates to support dart 2

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -1,8 +1,8 @@
 // Copyright (c) 2017, Devon Carew. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:test/src/utils.dart';
 import 'package:test/test.dart';
+import 'package:webdev/src/utils.dart';
 
 void main() {
   group('pluralize', () {
